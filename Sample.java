d1235c58de303ab277d59cd2ceb04caffe864764
西島昌太 <36064961+sarierinishi@users.noreply.github.com>
2019-04-17 21:13:28 +0900

    Add files via upload
    正規表現による整数判定

---
+++ Sample.java
@@ -0,0 +1,16 @@
+
+package sample;
+import java.util.regex.*;
+
+public class Sample {
+
+    public static void main(String[] args) {
+         final String input="-10+30-20+40+3-10+0";
+    final Pattern p = Pattern.compile("[\\d-]\\d*");
+    final Matcher m = p.matcher(input);
+    while(m.find()){
+      System.out.println(m.group());
+    }
+    }
+    
+}
